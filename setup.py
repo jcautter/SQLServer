36ca790418bfc6dca0900c3902a5dd6417c989fa
João Paulo Cautter <jcautter@gmail.com>
2023-12-23 20:56:22 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'SQLServer connection'
+LONG_DESCRIPTION = 'SQLServer connection'
+
+# Setting up
+setup(
+       # 'name' deve corresponder ao nome da pasta 'verysimplemodule'
+        name="SQLServer", 
+        version=VERSION,
+        author="João Paulo Cautter",
+        author_email="jcautter@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[pyodbc, numpy, pandas], # adicione outros pacotes que 
+        # precisem ser instalados com o seu pacote. Ex: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
